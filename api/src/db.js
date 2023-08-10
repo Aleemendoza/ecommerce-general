18a64975d09f56c07712724ca14c54b1a7245c59
Aleemendoza <alecolomendoza@gmail.com>
2023-08-09 21:56:35 -0300

    se saca db.js de gitignore

---
+++ api/src/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+
+const Sequelize = require('sequelize');
+/* const { singularize } = require('sequelize/types/lib/utils'); */
+
+const {
+  DB_USER, DB_PASSWORD, DB_HOST,
+} = process.env;
+
+
+function db() {
+
+  return new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/modaOutlet`, {
+    
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  });
+}
+
+module.exports = db;
